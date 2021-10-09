a634e1e4edaf140fc7999ce4c63aa66afb52b1d9
kertku <kertku@ttu.ee>
2021-10-09 23:19:59 +0300

    Add metric parameter to api calls

--- ICD0004 - Course Project/api_caller.py
+++ ICD0004 - Course Project/api_caller.py
@@ -2,16 +2,16 @@ import requests
 
 
 class ApiCalls:
-    def __init__(self, location, uri, api_key):
+    def __init__(self, location, uri, api_key, units='metric'):
         self.location = location
         self.uri = uri
         self.api_key = api_key
+        self.units = units
 
     def get_data_from_api(self):
-        api_call_parameters = {'q': self.location, 'appid': self.api_key}
+        api_call_parameters = {'q': self.location, 'appid': self.api_key, 'units': self.units}
         try:
             api_request = requests.get(self.uri, api_call_parameters)
             return api_request.json(), api_request.status_code
         except requests.ConnectionError as error:
-            print(error)
             return error
--- ICD0004 - Course Project/main.py
+++ ICD0004 - Course Project/main.py
@@ -1,5 +1,5 @@
 from api_caller import ApiCalls
 
 if __name__ == '__main__':
-    api = ApiCalls('Keila', 'https://api.openweathermap.org/data/2.5/weather?', '84e')
+    api = ApiCalls('Keila', 'https://api.openweathermap.org/data/2.5/weather?', '84e4560ea21a19b05777ab2b8db01083')
     print(api.get_data_from_api())
