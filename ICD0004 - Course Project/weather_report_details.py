90e6c34ee0b3eca52dc69914ee5aed823b7f280b
kertku <kertku@ttu.ee>
2021-10-09 23:33:54 +0300

    Add weather report details class

---
+++ ICD0004 - Course Project/weather_report_details.py
@@ -0,0 +1,5 @@
+class WeatherReportDetails:
+    def __init__(self, city, coordinates, temperature_units):
+        self.coordinates = coordinates
+        self.city = city
+        self.temperature_units = temperature_units
