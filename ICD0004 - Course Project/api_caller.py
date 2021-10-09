e8c352ba41705bcb572deeb5388479cf255925c0
kertku <kertku@ttu.ee>
2021-10-09 22:45:04 +0300

    Add the get data from api method

--- ICD0004 - Course Project/api_caller.py
+++ ICD0004 - Course Project/api_caller.py
@@ -6,3 +6,15 @@ class ApiCalls:
         self.location = location
         self.uri = uri
         self.api_key = api_key
+
+  def get_data_from_api(self):
+        api_call_parameters = {'q': self.location, 'appid': self.api_key}
+        try:
+            api_request = requests.get(self.uri, api_call_parameters)
+            if api_request.status_code == 200:
+                return api_request.json(), api_request.status_code
+            elif api_request.status_code == 401:
+                return
+        except requests.ConnectionError as error:
+            print(error)
+            return error
