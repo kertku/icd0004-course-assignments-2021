720f8a2019cecdfdf3d7d60aec2b2afa26accce1
kertku <kertku@ttu.ee>
2021-10-09 23:49:17 +0300

    Update unittests

--- ICD0004 - Course Project/tests/test_api_calls.py
+++ ICD0004 - Course Project/tests/test_api_calls.py
@@ -7,9 +7,9 @@ class TestClass:
     def test_api_call_returns_status_code_200_OK(self):
         api_call_returns_status_code_200_ok = ApiCalls('Keila', 'https://api.openweathermap.org/data/2.5/weather?',
                                                        '84e4560ea21a19b05777ab2b8db01083')
-        assert api_call_returns_status_code_200_ok.get_data_from_api()[1] == 200
+        assert api_call_returns_status_code_200_ok.get_data_from_api()['cod'] == 200
 
     def test_api_call_returns_status_code_401_unauthorized(self):
         api_call_with_wrong_api_key = ApiCalls('Keila', 'https://api.openweathermap.org/data/2.5/weather?',
                                                '84e')
-        assert api_call_with_wrong_api_key.get_data_from_api()[1] == 401
+        assert api_call_with_wrong_api_key.get_data_from_api()['cod'] == 401
