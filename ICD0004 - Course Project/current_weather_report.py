2e7ac03237a7449ebea5c366c9e85b399b8acbd3
kertku <kertku@ttu.ee>
2021-10-09 23:37:24 +0300

    Add current weather report class

---
+++ ICD0004 - Course Project/current_weather_report.py
@@ -0,0 +1,6 @@
+class CurrentWeatherReport:
+    def __init__(self, date, temperature, humidity, pressure):
+        self.date = date
+        self.temperature = temperature
+        self.humidity = humidity
+        self.pressure = pressure
