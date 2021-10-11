98a50938e34fb96a332adea25384f5b483320368
kertku <kertku@ttu.ee>
2021-10-11 18:25:33 +0300

    Update unittests, add BASE_URL and APPID constants

--- ICD0004 - Course Project/tests/test_api_calls.py
+++ ICD0004 - Course Project/tests/test_api_calls.py
@@ -1,15 +1,14 @@
-import pytest
 from api_caller import ApiCalls
 
 
 class TestClass:
+    APPID = '84e4560ea21a19b05777ab2b8db01083'
+    BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
-    def test_api_call_returns_status_code_200_OK(self):
-        api_call_returns_status_code_200_ok = ApiCalls('Keila', 'https://api.openweathermap.org/data/2.5/weather?',
-                                                       '84e4560ea21a19b05777ab2b8db01083')
+    def test_correct_api_call_returns_status_code_200_OK(self):
+        api_call_returns_status_code_200_ok = ApiCalls('Keila', TestClass.BASE_URL, TestClass.APPID)
         assert api_call_returns_status_code_200_ok.get_data_from_api()['cod'] == 200
 
     def test_api_call_returns_status_code_401_unauthorized(self):
-        api_call_with_wrong_api_key = ApiCalls('Keila', 'https://api.openweathermap.org/data/2.5/weather?',
-                                               '84e')
+        api_call_with_wrong_api_key = ApiCalls('Keila', TestClass.BASE_URL, '84e')
         assert api_call_with_wrong_api_key.get_data_from_api()['cod'] == 401
