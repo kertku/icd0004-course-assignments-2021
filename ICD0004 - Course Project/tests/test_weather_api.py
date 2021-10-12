20613e4e3793aba8dabb6beb471cbc7d07e8e0bf
kertku <kertku@ttu.ee>
2021-10-12 21:36:15 +0300

    Add weather api name not found returns 404 test

--- ICD0004 - Course Project/tests/test_weather_api.py
+++ ICD0004 - Course Project/tests/test_weather_api.py
@@ -4,10 +4,16 @@ from weater_api import WeatherApi
 
 
 class MyTestCase(unittest.TestCase):
+
     def test_weather_api_returns_name_when_correct_name_given(self):
         current_weather_report = WeatherApi("Keila")
         current_weather_report_to_dictionary = current_weather_report.get_current_weather_data()
-        assert current_weather_report_to_dictionary['name'] == 'Keila'  # add assertion here
+        self.assertEqual(current_weather_report_to_dictionary['name'], 'Keila')
+
+    def test_weather_api_returns_404_when_city_name_not_found(self):
+        current_weather_report = WeatherApi("Miskimidaeioleolemas")
+        current_weather_report_to_dictionary = current_weather_report.get_current_weather_data()
+        self.assertEqual(current_weather_report_to_dictionary['cod'], str(404))
 
 
 if __name__ == '__main__':
