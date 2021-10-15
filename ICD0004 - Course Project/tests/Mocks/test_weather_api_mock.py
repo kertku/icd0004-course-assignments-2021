7a9ce6f8b5392d4a5afd952078a0957948198941
kertku <kertku@ttu.ee>
2021-10-15 20:35:09 +0300

    Refactor weather api return correct name test with mocking

--- ICD0004 - Course Project/tests/Mocks/test_weather_api_mock.py
+++ ICD0004 - Course Project/tests/Mocks/test_weather_api_mock.py
@@ -2,17 +2,23 @@ import unittest
 from unittest import mock
 from unittest.mock import Mock, MagicMock, patch
 
+import self as self
+
 from weather_api import WeatherApi
 
 
 class TestWeatherApiMock(unittest.TestCase):
+    def setUp(self):
+        self.patcher = mock.patch('weather_api.WeatherApi.get_current_weather_data', return_value={"name": "Keila"})
+        self.patcher.start()
 
-    @patch('weather_api.WeatherApi.get_current_weather_data')
-    def test_weather_api_returns_name_when_correct_name_given(self, mock_get_data_from_api):
-        mock_get_data_from_api.return_value = {"name": "Keila"}
+    def test_weather_api_returns_name_when_correct_name_given(self):
         current_weather_report = WeatherApi("Keila")
         current_weather_report.get_current_weather_data()
         self.assertEqual(current_weather_report.get_city_name(), 'Keila')
+
+    def tearDown(self):
+        self.patcher.stop()
 
 
 if __name__ == '__main__':
